22965427859065b463b3be3c452d1fe868849c2d
David Carvalho <46721953+Davidcd1@users.noreply.github.com>
2026-03-28 17:33:01 -0300

    Adição de URL pra testes locais

--- script/config.js
+++ script/config.js
@@ -3,3 +3,8 @@ const CONFIG = {
     BASE_URL: "__API_URL_PLACEHOLDER__"
 };
 
+if (CONFIG.BASE_URL.includes("PLACEHOLDER") || window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+    CONFIG.BASE_URL = "http://localhost:8080/api"; 
+}
+
+console.log("🚀 MiniStock API conectada em:", CONFIG.BASE_URL);
